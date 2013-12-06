87063d9753259f82879be6ff25f4768d1a3cfee4
Giorgio Cefaro <giorgio.cefaro@gmail.com>
2013-12-06 20:07:58 +0100

    ideato/geo-adapter dependency removed, willdurand/geocoder dependency added

--- Manager/GeoCodeManager.php
+++ Manager/GeoCodeManager.php
@@ -2,8 +2,71 @@
 
 namespace PUGX\GeoFormBundle\Manager;
 
-use Geo\Search;
+use Geocoder\Geocoder;
+use Geocoder\Provider\ProviderInterface;
 
-class GeoCodeManager extends Search
+class GeoCodeManager
 {
+    protected $geoCoder;
+    protected $results;
+
+    public function __construct (Geocoder $geoCoder)
+    {
+        $this->geoCoder = $geoCoder;
+    }
+
+    /**
+     * Get results
+     *
+     * @return array
+     */
+    public function getResults()
+    {
+        return $this->results;
+    }
+
+    /**
+     * Query the chain of geo searching services
+     *
+     * @param string $q
+     * @param RuntimeException $e
+     */
+    public function query($q)
+    {
+        if (0 === count($this->geoCoder->getProviders())) {
+            throw new \RuntimeException('Service is not set');
+        }
+
+        $this->results = $this->geoCoder->geocode($q);
+    }
+
+    /**
+     * Get a specific result
+     *
+     * @param integer $index
+     * @return Geocoder\Result\ResultInterface
+     */
+    public function getResult($index)
+    {
+        if (!isset($this->results[$index])) {
+            return;
+        }
+
+        return $this->results[$index];
+    }
+
+    /**
+     * Get the first result
+     *
+     * @return Geocoder\Result\ResultInterface
+     */
+    public function getFirst()
+    {
+        return !isset($this->results[0])? : $this->results[0];
+    }
+
+    public function registerProvider(ProviderInterface $provider)
+    {
+        $this->geoCoder->registerProvider($provider);
+    }
 }
