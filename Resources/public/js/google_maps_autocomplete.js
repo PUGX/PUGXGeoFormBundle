98a3ae2c50c3d235cf2217766961d703841a9ca4
Massimiliano Arione <garakkio@gmail.com>
2017-07-27 20:26:06 +0200

    trigger change on lat/lng change. Fix #15

--- Resources/public/js/google_maps_autocomplete.js
+++ Resources/public/js/google_maps_autocomplete.js
@@ -1,15 +1,15 @@
-$.each($("form"), function(formindex, form) {
-    $.each($(form).find("input.pugx-geocode"), function(index, widget) {
+$.each($("form"), function (formindex, form) {
+    $.each($(form).find("input.pugx-geocode"), function (index, widget) {
 
         var options = {
-            types: ['geocode']
+            types: ["geocode"]
         };
 
         var autocomplete = new google.maps.places.Autocomplete(widget, options);
-        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        google.maps.event.addListener(autocomplete, "place_changed", function () {
             var place = autocomplete.getPlace();
-            $(widget).parents('form').find('input.pugx-geocode-latitude').val(place.geometry.location.lat());
-            $(widget).parents('form').find('input.pugx-geocode-longitude').val(place.geometry.location.lng());
+            $(widget).parents("form").find("input.pugx-geocode-latitude").val(place.geometry.location.lat()).trigger("change");
+            $(widget).parents("form").find("input.pugx-geocode-longitude").val(place.geometry.location.lng()).trigger("change");
         });
     });
 });
